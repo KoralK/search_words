325fb0aa33b97530e771a12398b392962189aa6c
Koral <koral@Korals-MacBook-Air.local>
2023-09-13 13:12:22 +0300

    initial commit

---
+++ search_tool.py
@@ -0,0 +1,41 @@
+import streamlit as st
+import os
+
+def search_filenames(term, root_dir=os.path.expanduser("~")):
+
+    """Search filenames for the term."""
+    matches = []
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            if term.lower() in filename.lower():
+                matches.append(os.path.join(root, filename))
+    return matches
+def search_file_content(term, root_dir=os.path.expanduser("~")):
+    """Search inside files for the term."""
+    matches = []
+    for root, dirs, files in os.walk(root_dir):
+        for filename in files:
+            file_path = os.path.join(root, filename)
+            with open(file_path, 'r', encoding='utf-8', errors='ignore') as f:
+                if term.lower() in f.read().lower():
+                    matches.append(file_path)
+    return matches
+
+# Streamlit UI
+st.title("File Search Tool")
+
+term = st.text_input("Enter the word or sequence of words to search:")
+search_type = st.radio("Where do you want to search?", ("In Filenames", "Inside File Content"))
+
+if st.button("Search"):
+    if search_type == "In Filenames":
+        results = search_filenames(term)
+    else:
+        results = search_file_content(term)
+
+    st.write("Results:")
+    for r in results:
+        st.write(r)
+
+if __name__ == "__main__":
+    pass
